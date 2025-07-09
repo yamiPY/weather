5ca5fb8e954809d6c48fc8b84533686bc96c25f8
mousa <alkdrwsmousa@gmail.com>
2025-07-09 13:00:47 +0300

    this is for title changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,15 @@
 import Weather from "./components/Weather";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-
+import Head from "next/head";
 export default function Home() {
-
-
   return (
     <>
-        <div className="flex justify-center items-center h-[100vh] bg-cyan-700">
-          <Weather />
-        </div>
-
+      <Head>
+        <title>Weather App</title>
+      </Head>
+      <div className="flex justify-center items-center h-[100vh] bg-cyan-700">
+        <Weather />
+      </div>
       ;
     </>
   );
