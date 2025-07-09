18348232e4d17b0d86b03e5eca2c92a70c1f99ff
mousa <alkdrwsmousa@gmail.com>
2025-07-09 13:02:54 +0300

    title changes and description

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,16 @@
 import Weather from "./components/Weather";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Head from "next/head";
+export const metadata = {
+  title: "Weather App",
+  description: "Weather jo app",
+};
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Weather App</title>
-      </Head>
       <div className="flex justify-center items-center h-[100vh] bg-cyan-700">
         <Weather />
       </div>
-      ;
     </>
   );
 }
